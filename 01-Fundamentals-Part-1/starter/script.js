9c41d2e7171dc43b759370a07d2afa9ff8e787b9
ms77grz <ms77grz@gmail.com>
2021-02-19 18:01:04 +0300

    Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+// NAMING CONVENTION
+let $meBe = true
+console.log($meBe)
+
+let _meBe = false
+console.log(_meBe)
+
+// DON NOT USE THIS VARIABLE NAMES
+let name = 'Samuel Jackson'
+console.log(name)
+
+let Person = 'Michael Jordan'
+console.log(Person)
+
+// CONSTANTS NAMING CONVENTION
+let PI = 3.1415
+console.log(PI)
+
+// USE DISCRIPTIVE VARIABLE NAMES
+let myFirstJob = 'Programmer'
+let myCurrentJob = 'Teacher'
+
+let job1 = 'programmer'
+let job2 = 'teacher'
