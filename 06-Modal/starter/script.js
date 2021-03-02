11a6925aa7b8b25ce5d50178d26d4b67ed60477d
Magomed Shamaev <ms77grz@gmail.com>
2021-03-02 22:44:55 +0300

    Modal - finished

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,25 @@
-'use strict';
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsShowModal = document.querySelectorAll('.show-modal')
+
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+btnsShowModal.forEach(btn => btn.addEventListener('click', openModal))
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', e => {
+  e.key === 'Escape' && !modal.classList.contains('hidden') && closeModal()
+})
