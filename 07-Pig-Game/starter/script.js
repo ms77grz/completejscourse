670f4d9fbd639d91f4ee384c3af6450d05a1e745
ms77grz <ms77grz@gmail.com>
2021-03-03 18:04:11 +0300

    Resseting the Game ...

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,19 +12,32 @@ const current1El = document.getElementById('current--1')
 
 const diceEl = document.querySelector('.dice')
 
-const btnNew = document.querySelector('btn--new')
+const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
-// STARTING CONDITIONS
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.classList.add('hidden')
+let scores, currentScore, activePlayer, playing
 
-const scores = [0, 0]
-let currentScore = 0
-let activePlayer = 0
-let playing = true
+// STARTING CONDITIONS
+const init = function () {
+  scores = [0, 0]
+  currentScore = 0
+  activePlayer = 0
+  playing = true
+
+  score0El.textContent = 0
+  score1El.textContent = 0
+  current0El.textContent = currentScore
+  current1El.textContent = currentScore
+
+  diceEl.classList.add('hidden')
+  player0El.classList.remove('player--winner')
+  player1El.classList.remove('player--winner')
+  player0El.classList.add('player--active')
+  player1El.classList.remove('player--active')
+}
+
+init()
 
 const switchPlayer = function () {
   // RESET THE CURRENT SCORE
@@ -85,4 +98,14 @@ btnHold.addEventListener('click', () => {
       switchPlayer()
     }
   }
+})
+
+btnNew.addEventListener('click', () => {
+  // RESETING INITIAL VALUES TO DEFAULTS
+  init()
+  //   scores[0] = 0
+  //   scores[1] = 0
+  //   playing = true
+  //   diceEl.classList.add('hidden')
+  //   activePlayer = 0
 })
