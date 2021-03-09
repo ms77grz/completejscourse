ef4e81b6b4d19ad30a3dd49e2dbb27492598c9b9
ms77grz <ms77grz@gmail.com>
2021-03-09 15:47:31 +0300

    Primitives VS. Objects in Practice - finished

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -190,7 +190,7 @@ function deleteShoppingCart() {
   console.log('All products deleted!')
 }
  */
-
+/*  
 //  this KEYWORD
 console.log(this)
 
@@ -226,3 +226,130 @@ const mark = {
 mark.calcAge = jonas.calcAge
 mark.calcAge()
 console.log(mark)
+*/
+
+// REGULAR FUNCTIONS VS. ARROW FUNCTIONS
+
+// ANOTHER REASON NOT TO USE VAR
+
+/* 
+var firstName = 'Maria' // IT WILL CREATE PROPERTIE ON THE GLOBAL OBJECT AND IT MIGHT BE DANGEROUS IN CERTAIN CASES
+
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this)
+    console.log(2021 - this.year)
+  },
+  // TO SHOW HERE THAT ARROW FUNCTION DOES NOT HAVE THIS KEYWORD
+  // NEVER USE AN ARROW FUNCTION AS A METHOD
+  greet: () => {
+    console.log(`Hey ${this.firstName}`)
+    console.log(this)
+  },
+  // INSTEAD USE A FUNCTION EXPRESSION
+  sayHello: function () {
+    console.log(`Hello ${this.firstName}`)
+  },
+}
+
+jonas.calcAge() // 30
+jonas.greet() // Hey undefined OR Hey Maria after creating var firstName = 'Maria' in the global scope
+jonas.sayHello()
+ */
+/* 
+// FUNCTION INSIDE OF A METHOD
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(2021 - this.year)
+
+    // // SOLUTION 1
+    // const self = this // self or that (PRE ES6 SOLUTION)
+    // const isMillenial = function () {
+    //   // console.log(this.year >= 1981 && this.year <= 1996) // TypeError: Cannot read property 'year' of undefined
+    //   // console.log(this) // undefined
+
+    //   console.log(self.year >= 1981 && self.year <= 1996) // true
+    // }
+    // isMillenial() // REGULAR FUNCTION CALL EVEN THOUGH IT HAPPENS INSIDE OF A METHOD
+
+    // MODERN SOLUTION
+    // ARROW FUNCTION WILL USE THIS KEYWORD OF ITS PARENT SCOPE
+    const isMillenial = () =>
+      console.log(this.year >= 1981 && this.year <= 1996)
+    isMillenial()
+  },
+}
+
+jonas.calcAge()
+ */
+/* 
+//  ARGUMENTS KEYWORD
+const addExpr = function (a, b) {
+  console.log(arguments)
+  return a + b
+}
+addExpr(2, 5) // [Arguments] { '0': 2, '1': 5 }
+addExpr(2, 5, 8, 12) // [Arguments] { '0': 2, '1': 5, '2': 8, '3': 12 }
+
+const addArrow = (a, b) => {
+  // console.log(arguments) // IN THE BROWSER - Uncaught ReferenceError: arguments is not defined
+  return a + b // REMEMBER WHEN WE HAVE MORE THAN ONE LINE OF CODE WE NEED TO EXPLICITLY RETURN
+}
+console.log('addArrow(2, 5, 8):', addArrow(2, 5, 8)) // 7
+ */
+
+// PRIMITIVES VS. OBJECTS
+/* 
+let age = 30
+let oldAge = age
+age = 31
+console.log(age) // 31
+console.log(oldAge) // 30
+
+const me = {
+  firstName: 'Jonas',
+  age: 30,
+}
+const friend = me
+friend.age = 27
+console.log('Friend:', friend) // Friend: { firstName: 'Jonas', age: 31 }
+console.log('Me:', me) // Me: { firstName: 'Jonas', age: 31 }
+ */
+
+// EACH PRIMITIVE VALUE WILL BE SAVED INTO ITS OWN PIECE OF MEMORY IN THE STACK
+let lastName = 'Williams'
+let oldLastName = lastName
+lastName = 'Davis'
+console.log(lastName) // Davis
+console.log(oldLastName) // Williams
+
+// OBJECT IS A REFERENCE VALUE AND IT'S GOING TO BE STORED IN THE HEAP AND THE STACK THEN JUST KEEPS A REFERENCE TO THE MEMORY POSITION
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+}
+
+const marriedJessica = jessica
+marriedJessica.lastName = 'Davis'
+console.log('Before marriage:', jessica) // Before marriage: { firstName: 'Jessica', lastName: 'Davis', age: 27 }
+console.log('After marriage:', marriedJessica) // After marriage: { firstName: 'Jessica', lastName: 'Davis', age: 27 }
+
+// WHAT IF WE WANT TO COPY AN OBJECT SO THAT WE CAN CHANGE ONE OF THEM WITHOUT CHANGING THE OTHER?
+// Object.assign() method ONLY creates a shallow copy and NOT a deep clone
+// a shallow copy will only copy the properties in the first level while a deep clone would copy everything
+
+const jessicaCopy = Object.assign({}, jessica)
+jessicaCopy.lastName = 'Jones'
+console.log('Before marriage:', jessica) // Before marriage: { firstName: 'Jessica', lastName: 'Davis', age: 27 }
+console.log('After marriage:', jessicaCopy) // After marriage: { firstName: 'Jessica', lastName: 'Jones', age: 27 }
+
+jessicaCopy.family.push('Mary')
+jessicaCopy.family.push('John')
+console.log('Before marriage:', jessica) // family: [ 'Alice', 'Bob', 'Mary', 'John' ]
+console.log('After marriage:', jessicaCopy) // family: [ 'Alice', 'Bob', 'Mary', 'John' ]
