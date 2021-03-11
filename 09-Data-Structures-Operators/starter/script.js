c5752407a0c73a6509b852a93d09aebaaca65c66
Magomed Shamaev <ms77grz@gmail.com>
2021-03-12 00:14:29 +0300

    Looping Arrays: The for-of Loop - finished

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -367,6 +367,7 @@ if (restaurant.orderPizza) {
 }
 restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
  */
+/* 
 // THE NULLISH COALESCING OPERATOR (??)
 restaurant.numGuests = 0;
 const guests = restaurant.numGuests || 10;
@@ -374,3 +375,108 @@ console.log(guests);
 // NULLISH: NULL AND UNDEFINED (NOT 0 OR '')
 const guestsCorrect = restaurant.numGuests ?? 10;
 console.log(guestsCorrect);
+ */
+/*  
+// CHALLENGE #1
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+*/
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+1. Create one player array for each team (variables 'players1' and 'players2')
+
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+GOOD LUCK 😀
+*/
+/* 
+// 1.
+const [players1, players2] = game.players;
+console.log(players1, players2);
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+// 5.
+// const { team1, x: draw, team2 } = game.odds;
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+// 6.
+const printGoals = function (...players) {
+  players.map(player => console.log(player));
+  console.log(arguments.length);
+};
+printGoals(...game.scored);
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+ */
+/* 
+//  LOOPING ARRAYS: THE FOR-OF LOOP
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu) console.log(item);
+
+// for (const item of menu.entries()) console.log(`${item[0] + 1}: ${item[1]}`);
+// USING DESTRUCTURING ASSIGNMENT
+for (const [index, item] of menu.entries())
+  console.log(`${index + 1}: ${item}`);
+
+console.log(...menu.entries());
+ */
+// ENHANCED OBJECT LITERALS
