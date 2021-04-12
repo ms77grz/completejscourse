3caf1616008945a01c217aaa0473f59db02dc4bb
Magomed Shamaev <ms77grz@gmail.com>
2021-04-12 08:34:45 +0300

    Selecting, Creating and Deleting Elements - 10%

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,17 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// CHAPTER: SELECTING, CREATING AND DELETING ELEMENTS
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
